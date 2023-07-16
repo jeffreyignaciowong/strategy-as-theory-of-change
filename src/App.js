42ff444e53bf6c876bd6b17a28242484d1e75db0
Jeffrey Wong <jeffreyignaciowong@gmail.com>
2023-07-16 16:45:44 -0400

    Remoev favicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Flow from './Flow';
 import { ReactFlowProvider } from "reactflow";
 import DnDFlow from "./DnDFlow";
 import { MantineProvider } from '@mantine/core';
